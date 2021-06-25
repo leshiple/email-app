ecaf6c98677fa895eba17f7b61838865a14ee2e0
leshiple <leshiple@gmail.com>
2021-06-25 09:35:58 +0300

    add en-US text

--- src/i18n/en-US/index.ts
+++ src/i18n/en-US/index.ts
@@ -1,7 +1,10 @@
-// This is just an example,
-// so you can safely delete all default props below
-
 export default {
-  failed: 'Action failed',
-  success: 'Action was successful',
+  compose: 'Compose',
+  folders: 'Folders',
+  inbox: 'Inbox',
+  starred: 'Starred',
+  draft: 'Draft',
+  sent: 'Sent mail',
+  trash: 'Trash',
+  labels: 'Labels',
 };
