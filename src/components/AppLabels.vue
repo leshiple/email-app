4425ca263dfc74954c3a751144d4a13b312d8240
leshiple <leshiple@gmail.com>
2021-06-25 09:39:12 +0300

    add component AppLabels

---
+++ src/components/AppLabels.vue
@@ -0,0 +1,39 @@
+<template>
+  <app-card-section-header title-slug="labels" />
+  <q-list dense>
+    <q-item
+      v-for="label in labels"
+      :key="label.name"
+      clickable
+      v-ripple
+    >
+      <q-item-section>{{ label.name }}</q-item-section>
+      <q-item-section avatar>
+        <q-icon :color="label.color" name="play_circle_filled" size="xs" />
+      </q-item-section>
+    </q-item>
+  </q-list>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import AppCardSectionHeader from 'src/components/AppCardSectionHeader.vue';
+
+export interface ILabel {
+  name: string
+  color: string
+}
+
+export default defineComponent({
+  name: 'AppLabels',
+  components: {
+    AppCardSectionHeader,
+  },
+  props: {
+    labels: {
+      type: Array as PropType<ILabel[]>,
+      required: true,
+    },
+  },
+});
+</script>
