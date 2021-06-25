b2133da7b141b502318835858109e645639c3262
leshiple <leshiple@gmail.com>
2021-06-25 09:41:23 +0300

    add sidebar to index page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,13 +1,18 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    Hello world
+  <q-page class="q-px-xl q-py-lg bg-grey-2">
+    <h1 class="text-h5">Email app</h1>
+    <div class="row items-start q-gutter-md">
+      <app-sidebar />
+    </div>
   </q-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import AppSidebar from 'src/components/AppSidebar.vue';
 
 export default defineComponent({
   name: 'PageIndex',
+  components: { AppSidebar },
 });
 </script>
