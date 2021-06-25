700063b94ddef1b642b66994bd47880de48d8f8e
leshiple <leshiple@gmail.com>
2021-06-25 09:37:06 +0300

    add component AppCardSectionHeader

---
+++ src/components/AppCardSectionHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="q-mx-md row items-center justify-start">
+    <div class="text-subtitle1 text-weight-bold">{{$t(titleSlug)}}</div>
+    <q-btn flat round color="grey-6" icon="add" size="10px" class="q-ml-xs" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'AppCardSectionHeader',
+  props: {
+    titleSlug: {
+      type: String,
+      required: true,
+    },
+  },
+  emits: {
+    click: null,
+  },
+});
+</script>
