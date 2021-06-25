ecff6ccc131318040ede11d3bad901d3f30773f1
leshiple <leshiple@gmail.com>
2021-06-25 09:37:34 +0300

    add component AppCompose

---
+++ src/components/AppCompose.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-card-section>
+    <q-btn color="primary" style="width: 100%">{{$t('compose')}}</q-btn>
+  </q-card-section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'AppCompose',
+});
+</script>
