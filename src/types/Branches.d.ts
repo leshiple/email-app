4ccfeffdcef8fd0be111aec884c56d0b08d93839
leshiple <leshiple@gmail.com>
2021-06-30 16:18:17 +0300

    add additional types to Branches

--- src/types/Branches.d.ts
+++ src/types/Branches.d.ts
@@ -1,12 +1,25 @@
-import { ILabel } from './Labels';
+import { ILabel } from './Labels.d';
+import { IMail } from './Mails';
 
-export interface IBranch {
+export interface IBranchBase {
   id: string
   lastMessageAuthor: string,
   subject: string,
   count: number,
   date: string,
   folder: string,
-  labels?: ILabel[],
   starred: boolean,
+}
+
+export interface IBranch extends IBranchBase {
+  labels: string[],
+}
+
+export interface IBranchWithLabels extends IBranchBase {
+  labels: ILabel[],
+}
+
+export interface IBranchExtended extends IBranchBase {
+  labels: ILabel[],
+  mails: IMail[]
 }
