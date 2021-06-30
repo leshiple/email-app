7bf45912355be39c2e115e985417e6ea5c7e56d2
leshiple <leshiple@gmail.com>
2021-06-30 16:51:03 +0300

    add branches getters: byFolder, byId, starred

--- src/store/branches/getters.ts
+++ src/store/branches/getters.ts
@@ -1,10 +1,46 @@
+import { IBranch, IBranchWithLabels } from 'src/types/Branches';
 import { GetterTree } from 'vuex';
 import { IRootState } from '../index';
 import { IBranchState } from './state';
 
+export type IGetBranchesByFolder = (folder: string) => IBranch[] // eslint-disable-line
+
 const getters: GetterTree<IBranchState, IRootState> = {
-  someAction(/* context */) {
-    // your code
+  starred(state, _childGetters, _rootState, rootGetters) {
+    return state.branches.reduce((acc: IBranchWithLabels[], branch: IBranch) => {
+      if (branch.starred) {
+        acc.push({
+          ...branch,
+          labels: branch.labels.map((label) => rootGetters['labels/byName'](label)), // eslint-disable-line
+        });
+      }
+      return acc;
+    }, []);
+  },
+  byId(state, _childGetters, _rootState, rootGetters) {
+    return (id: string) => {
+      const currentBranch = state.branches.find((branch) => branch.id === id);
+      if (currentBranch) {
+        return {
+          ...currentBranch,
+          labels: currentBranch.labels.map((label) => rootGetters['labels/byName'](label)), // eslint-disable-line
+          mails: rootGetters['mails/byBranch'](currentBranch.id), // eslint-disable-line
+        };
+      }
+      return currentBranch;
+    };
+  },
+  byFolder(state, _childGetters, _rootState, rootGetters) {
+    return (folder: string) => (
+      state.branches.reduce((acc: IBranchWithLabels[], branch: IBranch) => {
+        if (branch.folder === folder) {
+          acc.push({
+            ...branch,
+          labels: branch.labels.map((label) => rootGetters['labels/byName'](label)), // eslint-disable-line
+          });
+        }
+        return acc;
+      }, []));
   },
 };
 
