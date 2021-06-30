9fffc3ac747e2236335d649bb1b5ac3336fc60f0
leshiple <leshiple@gmail.com>
2021-06-30 16:25:36 +0300

    add page Starred

---
+++ src/pages/Starred.vue
@@ -0,0 +1,21 @@
+<template>
+  <app-folder folder="starred" :branches="branches"/>
+</template>
+
+<script lang="ts">
+import { defineComponent, inject } from 'vue';
+import AppFolder from 'src/components/AppFolder.vue';
+
+export default defineComponent({
+  name: 'PageStared',
+  components: {
+    AppFolder,
+  },
+  setup() {
+    const branches = inject('starredBranches');
+    return {
+      branches,
+    };
+  },
+});
+</script>
