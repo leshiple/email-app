823e71c5217f981fa646bd427aa68fa41aaee08c
leshiple <leshiple@gmail.com>
2021-07-01 22:45:43 +0300

    add redirect to inbox after delete folder

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -1,7 +1,7 @@
 <template>
   <q-card style="width: 230px;flex-shrink:0" class="q-pb-md">
     <app-compose />
-    <app-folders :folders="folders" :add="addFolder" :edit="editFolder" :delete="deleteFolder" />
+    <app-folders :folders="folders" :add="addFolder" :edit="editFolder" :delete="onDeleteFolder" />
     <q-separator inset class="q-mb-md" />
     <app-labels :labels="labels" :add="addLabel" :edit="editLabel" :delete="deleteLabel" />
   </q-card>
@@ -9,28 +9,36 @@
 
 <script lang="ts">
 import { defineComponent, inject } from 'vue';
+import { useRouter } from 'vue-router';
 import AppCompose from 'src/components/AppCompose.vue';
 import AppFolders from 'src/components/AppFolders.vue';
 import AppLabels from 'src/components/AppLabels.vue';
+import { IDeleteFolder } from 'src/types/Folders.d';
 
 export default defineComponent({
   name: 'AppSidebar',
   components: { AppCompose, AppFolders, AppLabels },
   setup() {
+    const router = useRouter();
     const folders = inject('folders');
     const addFolder = inject('addFolder');
     const editFolder = inject('editFolder');
-    const deleteFolder = inject('deleteFolder');
+    const deleteFolder = inject('deleteFolder') as IDeleteFolder;
     const labels = inject('labels');
     const addLabel = inject('addLabel');
     const editLabel = inject('editLabel');
     const deleteLabel = inject('deleteLabel');
 
+    const onDeleteFolder = async (folder: string) => {
+      deleteFolder(folder);
+      await router.push({ path: '/inbox' });
+    };
+
     return {
       folders,
       addFolder,
       editFolder,
-      deleteFolder,
+      onDeleteFolder,
       labels,
       addLabel,
       editLabel,
