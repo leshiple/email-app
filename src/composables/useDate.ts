63e5bf804580b48b65aaad2ba5642640156ef761
leshiple <leshiple@gmail.com>
2021-07-05 16:35:08 +0300

    add hours and minutes to format date

--- src/composables/useDate.ts
+++ src/composables/useDate.ts
@@ -4,7 +4,14 @@ import { useI18n } from 'vue-i18n';
 export default () => {
   const { locale } = useI18n({ useScope: 'global' });
   const formatDate = (date: number): ComputedRef<string> => computed(
-    () => new Intl.DateTimeFormat(locale.value).format(date),
+    () => new Intl.DateTimeFormat(locale.value, {
+      year: 'numeric',
+      month: 'numeric',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      hour12: false,
+    }).format(date),
   );
 
   return {
